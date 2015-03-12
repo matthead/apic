8a6b78eee722382089bcfc843f02cff6a39d7c9c
Matt <msb1320@gmail.com>
2015-03-12 18:33:32 -0400

    files

---
+++ UserDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webapicrud
+{
+    public class UserDto
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
+    }
+}
---
+++ ValuesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace webapicrud.Controllers
+{
+    public class UserController : ApiController
+    {
+        
+        [Route("api/login/")]
+        public IEnumerable<string> Get(string username,string password)
+        {
+           // return new string[] { "value1", "value2" };
+            UserHandler userHandler = new UserHandler();
+            return ConvertDictionaryToList(userHandler.Login(username,password));
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        //[Route("api/register")]
+        public string Put([FromBody]UserDto userDto)
+        {
+            return userDto.username;
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+        private List<string> ConvertDictionaryToList(Dictionary<string, string> values)
+        {
+            List<string> returnMessage = new List<string>();
+            foreach (KeyValuePair<string, string> kp in values)
+            {
+                string s = kp.Key + ":" + kp.Value;
+                returnMessage.Add(s);
+            }
+            return returnMessage;
+        }
+    }
+}
